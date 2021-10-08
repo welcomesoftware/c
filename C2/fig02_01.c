17dc5aaf0cbb1aa20c802c0ddd58b3b7c77b86c4
Welcome <welcomedario9@gmail.com>
2021-10-07 23:47:11 -0600

    created c2 dir

---
+++ C2/fig02_01.c
@@ -0,0 +1,15 @@
+/**
+ * Figura 2.1: fig02_01.c
+ * Un primer programa en c
+ */
+
+#include <stdio.h>
+
+/* Función principal */
+int main(void)
+{
+    printf("Bienvenido a C\n");
+    
+    // finaliza con éxito
+    return 0;
+}
